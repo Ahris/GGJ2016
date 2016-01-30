eef6285bd7e56ffa80de3df480792ef56ebb33a3
Ahris <alicewang54@gmail.com>
2016-01-30 11:40:11 -0600

    tank game compiles with DevKitPro

---
+++ build/gfx.d
@@ -0,0 +1,11 @@
+gfx.o: \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/source/gfx.c \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/screenmode.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gfx.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gba.h
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/screenmode.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gfx.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gba.h:
---
+++ build/main.d
@@ -0,0 +1,32 @@
+main.o: \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/source/main.c \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gba.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/screenmode.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/keypad.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gba.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gfx.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/logo.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/tank.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/text.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/objects.h \
+ /home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/star.h
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gba.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/screenmode.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/keypad.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gba.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/gfx.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/logo.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/tank.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/text.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/objects.h:
+
+/home/ahris/Dropbox/LinuxDocuments/SeniorWork/GameBoyAdvancedPrograms/GGJ2016/include/star.h:
